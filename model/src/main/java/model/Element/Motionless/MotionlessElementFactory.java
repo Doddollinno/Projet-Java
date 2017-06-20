1c59e7fa18d9487f3999c0fe045d665e434140ad
Revuxx <michael.desteuque@viacesi.fr>
2017-06-20 12:47:08 +0200

    ty

--- model/src/main/java/model/Element/Motionless/MotionlessElementFactory.java
+++ model/src/main/java/model/Element/Motionless/MotionlessElementFactory.java
@@ -1,5 +1,44 @@
 package model.Element.Motionless;
 
-public class MotionlessElementFactory {
+public abstract class MotionlessElementFactory {
+	private String DugDirt;
+	private String Dirt;
+	private String Wall;
+	private String Wallin;
+	
+	private static Wall Rectangle = new Wall();
+	private Dirt DIRT = new Dirt();
+	private DugDirt DUGDIRT = new DugDirt();
+	private Wallin WALL = new Wallin();
+
+
+	public abstract  MotionlessElement createWall();
+	{
+		
+	}
+
+	public MotionlessElement createDirt() {
+		// TODO - implement MotionlessElementsFactory.createDirt
+		throw new UnsupportedOperationException();
+	}
+
+	public MotionlessElement createDugDirt() {
+		// TODO - implement MotionlessElementsFactory.createDugDirt
+		throw new UnsupportedOperationException();
+	}
+
+	public MotionlessElement createWallin() {
+		// TODO - implement MotionlessElementsFactory.createWall
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param fileSymbol
+	 */
+	public MotionlessElement getFromfileSymbole(char fileSymbol) {
+		// TODO - implement MotionlessElementsFactory.getFromfileSymbole
+		throw new UnsupportedOperationException();
+	}
 
 }
