ac5ec8e41ad59e6b2f21d3cbc60215963e257c41
Loadren <sebagh_aaron@hotmail.com>
2017-06-23 16:15:00 +0200

    SpriteDone

--- model/src/main/java/model/Element/Sprite.java
+++ model/src/main/java/model/Element/Sprite.java
@@ -1,78 +1,132 @@
 package model.Element;
 
 import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Sprite {
 
-	private Image image;
-	private String imageName;
-	private int consoleImage;
-	private boolean imageLoaded;
+	 /** The image. */
+    private Image   image;
+
+    /** The image name. */
+    private String  imageName;
+
+    /** The console image. */
+    private char    consoleImage;
+
+    /** The is image loaded. */
+    private boolean imageLoaded;
+
+    /**
+     * Instantiates a new sprite.
+     *
+     * @param character
+     *            the character
+     * @param imageName
+     *            the image name
+     */
+    public Sprite(final char character, final String imageName) {
+        this.setConsoleImage(character);
+        this.setImageName(imageName);
+    }
+
+    /**
+     * Instantiates a new sprite.
+     *
+     * @param character
+     *            the character
+     */
+    public Sprite(final char character) {
+        this(character, "noimage.jpg");
+    }
+
+    /**
+     * Gets the image.
+     *
+     * @return the image
+     */
+    public final Image getImage() {
+        return this.image;
+    }
+    
+    /**
+     * Sets the image.
+     *
+     * @param image
+     *            the new image
+     */
+    private void setImage(final Image image) {
+        this.image = image;
+    }
+
+    /**
+     * Loads image.
+     *
+     * @throws IOException
+     *             Signals that an I/O exception has occurred.
+     */
+    public final void loadImage() throws IOException {
+        this.setImage(ImageIO.read(new File("Sprite/" + this.getImageName())));
+    }
+
+    /**
+     * Gets the console image.
+     *
+     * @return the consoleImage
+     */
+    public final char getConsoleImage() {
+        return this.consoleImage;
+    }
+
+    /**
+     * Sets the console image.
+     *
+     * @param consoleImage
+     *            the consoleImage to set
+     */
+    private void setConsoleImage(final char consoleImage) {
+        this.consoleImage = consoleImage;
+    }
+
+    /**
+     * Gets the image name.
+     *
+     * @return the imageName
+     */
+    public final String getImageName() {
+        return this.imageName;
+    }
+
+    /**
+     * Sets the image name.
+     *
+     * @param imageName
+     *            the imageName to set
+     */
+    private void setImageName(final String imageName) {
+        this.imageName = imageName;
+    }
 
 	/**
-	 * 
-	 * @param character
-	 * @param imageName
-	 */
-	public Sprite(char character, String imageName) {
-		// TODO - implement Sprite.Sprite
-		throw new UnsupportedOperationException();
-	}
+     * Checks if is image loaded.
+     *
+     * @return true, if is image loaded
+     */
+    public final boolean isImageLoaded() {
+        return this.imageLoaded;
+    }
 
 	/**
-	 * 
-	 * @param character
-	 */
-	public Sprite(char character) {
-		// TODO - implement Sprite.Sprite
-		throw new UnsupportedOperationException();
-	}
-
-	public Image getImage() {
-		return this.image;
-	}
-
-	public void loadImage() {
-		// TODO - implement Sprite.loadImage
-		throw new UnsupportedOperationException();
-	}
-
-	public char getConsoleImage() {
-		// TODO - implement Sprite.getConsoleImage
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 * 
-	 * @param ConsoleImage
-	 */
-	private void setConsoleImage(char ConsoleImage) {
-		// TODO - implement Sprite.setConsoleImage
-		throw new UnsupportedOperationException();
-	}
-
-	public String getImageName() {
-		return this.imageName;
-	}
-
-	/**
-	 * 
-	 * @param imageName
-	 */
-	private void setImageName(String imageName) {
-		this.imageName = imageName;
-	}
-
-	public boolean isImageLoaded() {
-		return this.imageLoaded;
-	}
-
-	/**
-	 * 
-	 * @param isImageLoaded
-	 */
-	public void setImageLoaded(boolean isImageLoaded) {
-		this.imageLoaded = isImageLoaded;
-	}
+     * Sets the image loaded.
+     *
+     * @param isImageLoaded
+     *            the new image loaded
+     */
+    public final void setImageLoaded(final boolean isImageLoaded) {
+        this.imageLoaded = isImageLoaded;
+    }
 
 }
